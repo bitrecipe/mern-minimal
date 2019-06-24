3ee133eaa4a71198878e979a5802ced7e7301684
bitrecipe <subeeshmm.open@gmail.com>
2019-06-24 20:16:38 +0530

    reducers hot reloading

--- src/client/store.js
+++ src/client/store.js
@@ -22,5 +22,12 @@ export function configureStore(initialState = {}) {
 
     const store = createStore(rootReducer, initialState, compose(...enhancers));
 
+    if (module.hot) {
+        module.hot.accept('./reducers', () => {
+            const nextRootReducer = require('./reducers').default;
+            store.replaceReducer(nextRootReducer);
+        });
+    }
+
     return store;
 }
